0659f875da6003210d57e76dcb87e95cc361a233
Ethan Allnutt <allnuttethan@gmail.com>
2017-04-11 09:16:02 -0400

    working on post

--- app.js
+++ app.js
@@ -265,6 +265,13 @@ app.get('/logout', function(req,res) {
 
 });
 
+app.post('/padmin', function(req, res) {
+    console.log(req.query.token);
+    res.writeHead(303, {"Location": "/"});
+    res.end();
+    res.send();
+});
+
 app.get('/admin', function(req,res) {
 
     /*
