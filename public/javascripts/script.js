eb3edd5dd74052b68ec486694a9dcd647674a1a2
Ethan Allnutt <allnuttethan@gmail.com>
2017-04-11 19:13:45 -0400

    trying to use cookies for info

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,13 +1,66 @@
 
 function getUserToken() {
-    return sessionStorage.getItem('userID');
+    try {
+        sessionStorage.setItem('test',1);
+        sessionStorage.removeItem('test');
+        return sessionStorage.getItem('userID');
+    } catch (err) {
+        return getCookie('userID');
+    }
+
 }
 
 function getUserEmail() {
-    return sessionStorage.getItem('userEmail');
+    try {
+        sessionStorage.setItem('test',1);
+        sessionStorage.removeItem('test');
+        return sessionStorage.getItem('userEmail');
+    } catch (err) {
+        return getCookie('userEmail');
+    }
 }
 
 function adminLink() {
     var userToken = getUserToken();
     document.location.href = "/admin?token=" + userToken;
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie(cname) {
+    var user = getCookie(cname);
+    if (user != "") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function deleteCookie(cname) {
+    if(checkCookie(cname)) {
+        var d = new Date();
+        d.setTime(d.getTime()-100000);
+        var expires = "expires="+d.toUTCString();
+        document.cookie = cname + "=;" + expires + ";path=/";
+    }
 }
