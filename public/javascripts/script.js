88ae4efe2c525657958f563479483f1720f9d761
Ethan Allnutt <allnuttethan@gmail.com>
2017-04-10 20:46:00 -0400

    playing with auth

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,1 +1,12 @@
 var googleUser = {};      //So we have 'global' access to user's data and not just in the scope of the login function
+
+String.prototype.hashCode = function() {
+    var hash = 0;
+    if (this.length == 0) return hash;
+    for (i = 0; i < this.length; i++) {
+        char = this.charCodeAt(i);
+        hash = ((hash << 5) - hash) + char;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+}
