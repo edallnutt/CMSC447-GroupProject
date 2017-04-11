eaf6aecb4ad1d0e8feca4e8f62a7a23ad7117fcb
Ethan Allnutt <allnuttethan@gmail.com>
2017-04-11 15:07:01 -0400

    playing with downloading a file of nums

--- app.js
+++ app.js
@@ -299,9 +299,8 @@ app.get('/number-list', function(req, res) {
     var file = require('./data.json');
     //console.log(file);
     for(var email in file) {
-        //console.log(file[email]);
         for(var sub in file[email]) {
-            console.log(file[email][sub]);
+            console.log(file[email][sub].alias+" : "+file[email][sub].num_submit);
         }
     }
     res.send();
