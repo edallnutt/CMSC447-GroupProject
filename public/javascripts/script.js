d0536a69b230a5e3dd7be47024d53abfb438eec4
Ethan Allnutt <allnuttethan@gmail.com>
2017-04-13 11:39:50 -0400

    working on a searchable table

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,13 +1,11 @@
 
 function searchTable() {
-    // Declare variables
     var input, filter, table, tr, td, i;
     input = document.getElementById("tableSearch");
     filter = input.value.toUpperCase();
     table = document.getElementById("numTable");
     tr = table.getElementsByTagName("tr");
 
-    // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
         td = tr[i].getElementsByTagName("td")[0];
         if (td) {
