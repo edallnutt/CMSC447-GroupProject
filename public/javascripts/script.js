2ac6ff39f960c038a19b890ad41f9a054eb08f52
Ethan Allnutt <allnuttethan@gmail.com>
2017-04-13 11:45:16 -0400

    working on a searchable table

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,9 +1,9 @@
 
-function searchTable() {
+function searchTable(searchID, tableID) {
     var input, filter, table, tr, td, i;
-    input = document.getElementById("tableSearch");
+    input = document.getElementById(searchID);
     filter = input.value.toUpperCase();
-    table = document.getElementById("numTable");
+    table = document.getElementById(tableID);
     tr = table.getElementsByTagName("tr");
 
     for (i = 0; i < tr.length; i++) {
@@ -18,8 +18,42 @@ function searchTable() {
     }
 }
 
-
-
+function sortTable(col, tableID) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById(tableID);
+    switching = true;
+    dir = "asc";
+    while (switching) {
+        switching = false;
+        rows = table.getElementsByTagName("TR");
+        for (i = 1; i < (rows.length - 1); i++) {
+            shouldSwitch = false;
+            x = rows[i].getElementsByTagName("TD")[col];
+            y = rows[i + 1].getElementsByTagName("TD")[col];
+            if (dir == "asc") {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    shouldSwitch= true;
+                    break;
+                }
+            } else if (dir == "desc") {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    shouldSwitch= true;
+                    break;
+                }
+            }
+        }
+        if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            switchcount ++;
+        } else {
+            if (switchcount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
+            }
+        }
+    }
+}
 
 function getUserToken() {
     try {
